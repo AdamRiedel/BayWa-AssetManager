e5a4522d77e2c2e703106c201fbcaf925ecaf219
AdamRiedel <Adam.Riedel@baywa.de>
2025-02-11 14:42:56 +0100

    Link it in the main.jsx-file

--- baywa-assetmanager/src/main.jsx
+++ baywa-assetmanager/src/main.jsx
@@ -3,17 +3,18 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import HomeRoute from "./routes/Home.routes.jsx";
+import AssetsRoute from "./routes/Assets.routes.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Suspense>
         <Routes>
-          <Route />
+          <Route path="/" element={<HomeRoute />} />
+          <Route path="/assets" element={<AssetsRoute />} />
         </Routes>
       </Suspense>
     </BrowserRouter>
-
-    <App />
   </StrictMode>
 );
