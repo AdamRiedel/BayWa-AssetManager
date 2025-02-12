7081ad401360c3d9caf494b3b5a12987e4faebeb
AdamRiedel <Adam.Riedel@baywa.de>
2025-02-12 10:57:10 +0100

    Style the component and link the file

--- baywa-assetmanager/src/components/CreateAsset.comp.jsx
+++ baywa-assetmanager/src/components/CreateAsset.comp.jsx
@@ -1,6 +1,7 @@
 import { useContext, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import validator from "validator";
+import "./CreateAsset.style.css";
 
 export default function CreateAsset() {
   //const { polls, setPolls } = useContext(true);
