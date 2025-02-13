0b5ad47f56a13a4a69598d58e51466ac0615310c
AdamRiedel <Adam.Riedel@baywa.de>
2025-02-13 14:42:31 +0100

    Init code for fetching the api (docker)

--- baywa-assetmanager/src/App.jsx
+++ baywa-assetmanager/src/App.jsx
@@ -1,7 +1,37 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 import { NavBar } from "./components/NavBar.comp.jsx";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    onAPIFetch();
+    console.log(data);
+  }, []);
+
+  const onAPIFetch = async () => {
+    try {
+      setLoading(true);
+      const request = await fetch("http://localhost:3000/assets");
+      if (!request.ok) {
+        throw new Error("request failed");
+      }
+      const result = await request.json();
+      setData(result);
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+      console.log(error, error.message);
+    } finally {
+      setTimeout(() => {
+        setLoading(false);
+      }, 3000);
+    }
+  };
+
   return (
     <>
       <NavBar />
